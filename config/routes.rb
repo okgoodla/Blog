7c3204a6a95364c8ec4696dd49f58954ffaad5ab
Simon Leung <okgoodla@gmail.com>
2015-08-16 14:20:30 +0000

    Final

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  resources :posts do
+    resources :comments
+  end
+  
+  
+  root "posts#index"
+  
+  get '/about', to: 'pages#about'
+  
+end
+
+
+
